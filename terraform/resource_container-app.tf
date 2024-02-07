6b1737a7815bb5a6e3c00875a83d02edc5a9473a
John Thuneby <thuneby@yahoo.dk>
2024-02-07 16:07:32 +0100

    add liveness probe

--- terraform/resource_container-app.tf
+++ terraform/resource_container-app.tf
@@ -23,6 +23,10 @@ resource "azurerm_container_app" "application" {
       image  = "crthunebyinfrastructure.azurecr.io/albumapi:latest"
       cpu    = 0.25
       memory = "0.5Gi"
+      liveness_probe {
+        port      = 8080
+        transport = "HTTP"
+      }
     }
   }
 
